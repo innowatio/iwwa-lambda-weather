d56edabaac29c06c4b90c599f9a20f6da3badebe
Manuel Serra <nitroin@hotmail.com>
2016-06-14 17:58:34 +0200

    Add dispatch logs

--- src/steps/dispatch-events.js
+++ src/steps/dispatch-events.js
@@ -1,11 +1,14 @@
-import {dispatchEvent} from "../services/dispatcher";
 import moment from "moment";
+
+import {dispatchEvent} from "../services/dispatcher";
+import log from "../services/logger";
 
 export function dispatchEvents(weathers) {
     const now = moment.utc().valueOf();
     const normalized = moment.utc(now - (now % (1000 * 60 * 60 * 2)));
 
     weathers.forEach(weather => {
+        log.info(`${weather.country}-${weather.provincia.toLowerCase()}`, "dispatch event");
         dispatchEvent("element inserted in collection readings", {
             sensorId: `${weather.country}-${weather.provincia.toLowerCase()}`,
             date: normalized.format(),
