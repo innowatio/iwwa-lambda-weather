c4d38bd79195b92fa60338f49a601375d231713e
Manuel Serra <nitroin@hotmail.com>
2016-06-14 17:10:11 +0200

    Add Kinesis configuration parameters

--- src/config.js
+++ src/config.js
@@ -3,3 +3,5 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export const OPEN_WEATHER_APP_ID = process.env.OPEN_WEATHER_APP_ID;
+export const KINESIS_STREAM = process.env.KINESIS_STREAM;
+export const KINESIS_PRODUCER_ID = process.env.KINESIS_PRODUCER_ID;
