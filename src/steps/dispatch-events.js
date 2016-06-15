76ce07a3741f3ff8476f2a824ffdd572fff584b7
Manuel Serra <nitroin@hotmail.com>
2016-06-15 09:52:01 +0200

    Change dispatched event

--- src/steps/dispatch-events.js
+++ src/steps/dispatch-events.js
@@ -8,28 +8,31 @@ export function dispatchEvents(weathers) {
     const normalized = moment.utc(now - (now % (1000 * 60 * 60 * 2)));
 
     weathers.forEach(weather => {
-        log.info(`${weather.country}-${weather.provincia.toLowerCase()}`, "dispatch event");
-        dispatchEvent("element inserted in collection readings", {
-            sensorId: `${weather.country}-${weather.provincia.toLowerCase()}`,
-            date: normalized.format(),
-            measurements: [{
-                type: "weather-cloudeness",
-                value: weather.cloudeness,
-                unitOfMeasurement: "%"
-            }, {
-                type: "weather-humidity",
-                value: weather.humidity,
-                unitOfMeasurement: "%"
-            }, {
-                type: "weather-temperature",
-                value: weather.temperature,
-                unitOfMeasurement: "°C"
-            }, {
-                type: "weather-id",
-                value: weather.id,
-                unitOfMeasurement: "id"
-            }]
-        }, {});
+        const event = {
+            element: {
+                sensorId: `${weather.country}-${weather.provincia.toLowerCase()}`,
+                date: normalized.format(),
+                measurements: [{
+                    type: "weather-cloudeness",
+                    value: weather.cloudeness,
+                    unitOfMeasurement: "%"
+                }, {
+                    type: "weather-humidity",
+                    value: weather.humidity,
+                    unitOfMeasurement: "%"
+                }, {
+                    type: "weather-temperature",
+                    value: weather.temperature,
+                    unitOfMeasurement: "°C"
+                }, {
+                    type: "weather-id",
+                    value: weather.id,
+                    unitOfMeasurement: "id"
+                }]
+            }
+        };
+        log.info(event, "dispatch event");
+        dispatchEvent("element inserted in collection readings", event);
     });
 }
 
